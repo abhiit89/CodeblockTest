ccbfa611a22935369f17b03ef6a11e2e5d5e3cb8
Abhinav <abhi.aec89@gmail.com>
2016-02-08 23:23:56 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+//#include<conio.h>
+using namespace std;
+class Student{
+
+int a = 0;
+int b = 0;
+
+};
+
+Student::add(int a, int b)
+{
+return a+b;
+}
+int main()
+{
+    cout << "Hello world!" << endl;
+    cin>>a;
+    cout<<"\n";
+    cin>>b;
+    Student as = new Student();
+    as.a= a;
+    as.b = b;
+    c = as.add(a,b);
+    cout<<c;
+    return 0;
+}
